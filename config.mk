0736b56b9b102a10bc0c5eadf25309d770d2bfec
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-10-25 21:32:38 -0400

    Added mpd support.

--- config.mk
+++ config.mk
@@ -10,12 +10,15 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
+MPDLIB  =  -lmpdclient
+MPDFLAG =  -DMPD
+
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${MPDLIB}
 
 # flags
-CPPFLAGS = -DVERSION=\"${VERSION}\"
+CPPFLAGS = -DVERSION=\"${VERSION}\" ${MPDFLAG}
 CFLAGS = -ggdb -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 #CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = -ggdb ${LIBS}
--- dwmstatus.c
+++ dwmstatus.c
@@ -12,6 +12,7 @@
 #include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <mpd/client.h>
 
 #include <X11/Xlib.h>
 
@@ -64,17 +65,19 @@ getbattery(){
 		fclose(fp);
 		batpercent = (lnum1/(lnum2/100));
 		if (strcmp(status,"Charging") == 0)
-			s = "<span color=\""BLUE"\">"BAT_CHARGING_GLYPH"</span>";
+			s = "<span color=\""YELLOW"\">"BAT_CHARGING_GLYPH"</span>";
 		if (strcmp(status,"Discharging") == 0) {
-			if (batpercent >= 70) {
+			if (batpercent >= 75) {
+				s = "<span color=\""BLUE"\">"BAT_FULL_GLYPH"</span>";
+			} else if (batpercent >= 50) {
 				s = "<span color=\""BLUE"\">"BAT_70_PERCENT_GLYPH"</span>";
-			} else if (batpercent >= 30) {
+			} else if (batpercent >= 15) {
 				s = "<span color=\""MAGENTA"\">"BAT_30_PERCENT_GLYPH"</span>";
 			} else {
 				s = "<span color=\""RED"\">"BAT_EMPTY_GLYPH"</span>";
 			}
 		}
-		if (strcmp(status,"Full") == 0) 
+		if (strcmp(status,"Full") == 0)
 			s = "<span color=\""BLUE"\">"BAT_FULL_GLYPH"</span>";
 		return smprintf("%s %ld%%", s, batpercent);
 	}
@@ -143,22 +146,59 @@ get_signal_strength()
 	return smprintf("%s %d", glyph, strength);
 }
 
+char *
+get_mpd_stat() {
+	struct mpd_song * song = NULL;
+	const char * title = NULL;
+	const char * artist = NULL;
+	char * retstr = NULL;
+	struct mpd_connection * conn ;
+	if (!(conn = mpd_connection_new("localhost", 0, 30000)) ||
+	    mpd_connection_get_error(conn)){
+		return smprintf("");
+	}
+
+	mpd_command_list_begin(conn, true);
+	mpd_send_status(conn);
+	mpd_send_current_song(conn);
+	mpd_command_list_end(conn);
+
+	struct mpd_status* theStatus = mpd_recv_status(conn);
+	if ((theStatus) && (mpd_status_get_state(theStatus) == MPD_STATE_PLAY)) {
+		mpd_response_next(conn);
+		song = mpd_recv_song(conn);
+		title = smprintf("%s",mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
+		artist = smprintf("%s",mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
+
+		mpd_song_free(song);
+		retstr = smprintf("%s %s - %s",
+		                  "<span color=\""MAGENTA"\">"MUSIC_GLYPH"</span>",
+		                  artist, title);
+		free((char*)title);
+		free((char*)artist);
+	}
+	else retstr = smprintf(MUSIC_GLYPH);
+	mpd_response_finish(conn);
+	mpd_connection_free(conn);
+	return retstr;
+}
+
 void
 setstatus(char *str)
 {
 	XStoreName(dpy, DefaultRootWindow(dpy), str);
 	XSync(dpy, False);
 }
- 
+
 void term(int signum)
 {
-    term_request = True;
+	term_request = True;
 }
 
 int
 main(void)
 {
-	
+
 	struct sigaction sa;
 	memset(&sa, 0, sizeof(struct sigaction));
 	sa.sa_handler = term;
@@ -171,21 +211,25 @@ main(void)
 	char *tcan;
 	char *strength;
 	char *bat;
+	char *mpdstring;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
 		return 1;
 	}
 
-	for (; !term_request; sleep(2)) {
+	for (; !term_request; sleep(3)) {
 		tcan= get_time("%H:%M", tcanada);
 		strength = get_signal_strength();
 		bat = getbattery();
-		status = smprintf(" %s %s %s %s %s %s ",
-		                  POWERLINE_SOFT_RIGHT, strength, 
-		                  POWERLINE_SOFT_RIGHT, bat, 
+		mpdstring = get_mpd_stat();
+		status = smprintf(" %s %s %s %s %s %s %s %s ",
+		                  POWERLINE_SOFT_RIGHT, mpdstring,
+		                  POWERLINE_SOFT_RIGHT, strength,
+		                  POWERLINE_SOFT_RIGHT, bat,
 		                  POWERLINE_SOFT_RIGHT, tcan);
 		setstatus(status);
+		free(mpdstring);
 		free(bat);
 		free(strength);
 		free(tcan);
