d61b517df3f1ae1113f9d288f9a09825eeea28c3
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-10-27 16:48:37 -0400

    Link against glib

--- config.mk
+++ config.mk
@@ -10,19 +10,23 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-MPDLIB  =  -lmpdclient
-MPDFLAG =  -DMPD
+MPDLIB = $(shell pkg-config --libs libmpdclient)
+MPDFLAG = -DMPD
+
+GLIBLIB = $(shell pkg-config --libs glib-2.0)
+GLIBFLAG = $(shell pkg-config --cflags glib-2.0)
+
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${MPDLIB}
+INCS = -I. -I/usr/include -I${X11INC} ${GLIBFLAG}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${MPDLIB} ${GLIBLIB}
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" ${MPDFLAG}
-CFLAGS = -ggdb -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-#CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS = -ggdb ${LIBS}
-#LDFLAGS = -s ${LIBS}
+# CFLAGS = -ggdb -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+# LDFLAGS = -ggdb ${LIBS}
+LDFLAGS = -s ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
