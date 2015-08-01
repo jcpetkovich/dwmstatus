ae07302172b7b934490850a042f50dcb901af56b
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-08-01 14:56:57 -0400

    Add dirlocals.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+(
+ (nil . ((flycheck-gcc-language-standard . "c99")
+         (flycheck-gcc-include-path . ("/usr/include/glib-2.0/" "/usr/lib64/glib-2.0/include/"))
+         (flycheck-clang-language-standard . "c99")
+         (flycheck-clang-include-path . ("/usr/include/glib-2.0/" "/usr/lib64/glib-2.0/include/")))
+      )
+ )
